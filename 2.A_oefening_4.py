2a89512a5b4df8b622c50ca8769dec6ed7f06615
fenvanveldeke <lombardirfenne@gmail.com>
2026-09-14 11:31:07 +0000

    oefening 4

---
+++ 2.A_oefening_4.py
@@ -0,0 +1,5 @@
+huidige_geboortejaar = input("geef geboortejaar :")
+huidige_geboortejaar = int(huidige_geboortejaar)
+jaartal = 2030
+leeftijd_toekomst = jaartal - huidige_geboortejaar
+print(f"je leeftijd in {jaartal} is {leeftijd_toekomst}")
