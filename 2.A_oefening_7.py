f06947c1e39edcff33722e066f1f48af216b9d9b
fenvanveldeke <lombardirfenne@gmail.com>
2026-09-14 12:04:55 +0000

    oefn

---
+++ 2.A_oefening_7.py
@@ -0,0 +1,5 @@
+graden_fratenheim = input("geef graden in fratenheim:")
+graden_fratenheim = int(graden_fratenheim)
+product_1 = graden_fratenheim - 32
+celcius = product_1 * 5/9
+print(f"dat is {celcius}")
