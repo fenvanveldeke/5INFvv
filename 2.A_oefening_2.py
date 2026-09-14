1ab19c6c76f32170c7e2ca5c83826624883c8403
fenvanveldeke <lombardirfenne@gmail.com>
2026-09-14 11:15:00 +0000

    oefening 2

---
+++ 2.A_oefening_2.py
@@ -0,0 +1,6 @@
+tekst = "Phyton programmeren is leuk!"
+print(tekst.upper())
+print(tekst.replace("leuk","geweldig"))
+zin_twee = (tekst.replace("leuk","geweldig"))
+aantal=len(zin_twee)
+print(aantal)
