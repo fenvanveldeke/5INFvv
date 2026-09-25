649c3147e48f0a82456fbfff9dad12585bebc083
fenvanveldeke <lombardirfenne@gmail.com>
2026-09-25 11:17:58 +0000

    oefening 3 "n 4

---
+++ 3.A_oefening_3.py
@@ -0,0 +1,8 @@
+import random
+getal = random.randint(1, 10)
+raad = int(input("geef een getal tussen 1 en 10"))
+if getal == raad:
+    print("goed gedaan")
+else:
+    print(f"fout het was {getal}")5
+    
---
+++ 3.A_oefening_4.py
@@ -0,0 +1,7 @@
+getal = int(input("Geef een getal "))
+if getal >0:
+    print("je getal is positief")
+else:
+    print("je getal is negatief")
+if getal == 0:
+    print("je getal is 0")
